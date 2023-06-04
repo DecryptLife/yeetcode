53d249e018ea7b398dcf886d0c82d595bca63d51
DecryptLife <benson.thomas2602@gmail.com>
2023-06-04 00:19:29 -0500

    Evaluate Reverse Polish Notation

--- src/ReversePolishNotation.java
+++ src/ReversePolishNotation.java
@@ -1,7 +1,58 @@
+import java.util.Stack;
+
 public class ReversePolishNotation {
+
+    public static int evalRPN(String[] tokens) {
+
+        Stack<Integer> stack = new Stack<>();
+        int a=0;
+        int b=0;
+        for(String token:tokens)
+        {
+
+            switch(token){
+                case "+":
+                    a = stack.pop();
+                    b = stack.pop();
+
+                    stack.push(a+b);
+
+                    break;
+                case "-":
+                    a = stack.pop();
+                    b = stack.pop();
+
+                    stack.push(b-a);
+                    break;
+                case "/":
+
+                    a = stack.pop();
+                    b = stack.pop();
+                    stack.push(b/a);
+                    break;
+                case "*":
+                    a = stack.pop();
+                    b = stack.pop();
+
+                    stack.push(a*b);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
 
     public static void main(String[] args) {
 
-        String[] exp = {"3","3","+"};
+        String[] exp1 = {"2","1","+","3","*"};
+        String[] exp2 = {"4","13","5","/","+"};
+        String[] exp3 = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+
+        System.out.println(evalRPN(exp1));
+        System.out.println(evalRPN(exp2));
+        System.out.println(evalRPN(exp3));
+
     }
 }
